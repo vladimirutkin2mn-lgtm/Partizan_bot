d9830ae228b9fc431f5efdc2e6714fc967a5b8db
vladimirutkin2mn-lgtm <vladimir.utkin2mn@gmail.com>
2026-08-16 09:34:46 +0300

    Remove retired service resets from autonomy overview tests

--- tests/test_autonomy_overview.py
+++ tests/test_autonomy_overview.py
@@ -11,14 +11,12 @@ from app.autonomous_growth import (
 from app.autonomous_growth_control import AUTONOMOUS_GROWTH_CONTROL_AUDIT_NAMESPACE
 from app.autonomous_paid import AUTONOMOUS_PAID_ACTIVATION_AUDIT_NAMESPACE
 from app.autonomy_service import growth_mandate_service
-from app.channel_service import channel_service
 from app.distribution_analytics_service import distribution_analytics_service
 from app.distribution_control_plane_service import distribution_control_plane_service
 from app.distribution_execution_schemas import DistributionActionExecutionRequest
 from app.distribution_execution_service import distribution_execution_service
 from app.distribution_growth_manager_service import distribution_growth_manager_service
 from app.distribution_play_service import distribution_play_service
-from app.growth_play_service import growth_play_service
 from app.icp_service import icp_service
 from app.main import app
 from app.paid_campaign import paid_campaign_spec_service
@@ -32,9 +30,7 @@ client = TestClient(app)
 def reset_state() -> None:
     product_intake_service.reset()
     icp_service.reset()
-    channel_service.reset()
     audience_intelligence_service.reset()
-    growth_play_service.reset()
     distribution_play_service.reset()
     distribution_control_plane_service.reset()
     distribution_execution_service.reset()
