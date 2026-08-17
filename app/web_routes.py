28d084afe3a3d277abdc25cec7a51f8e307f6de5
vladimirutkin2mn-lgtm <vladimir.utkin2mn@gmail.com>
2026-08-17 23:19:09 +0300

    Serve customer Autopilot stylesheet

--- app/web_routes.py
+++ app/web_routes.py
@@ -42,6 +42,7 @@ _LANDING_ASSETS = {
 }
 _START_ASSETS = {
     "start.v1.css": "text/css; charset=utf-8",
+    "start.autopilot.v1.css": "text/css; charset=utf-8",
     "start.v1.js": "text/javascript; charset=utf-8",
 }
 
