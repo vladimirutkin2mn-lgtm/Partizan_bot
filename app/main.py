ac501c16679a0a34f3c10cb151ecf38fd8514c0b
vladimirutkin2mn-lgtm <vladimir.utkin2mn@gmail.com>
2026-09-15 10:32:51 +0300

    Wire Growth Balance operator reconciliation route

--- app/main.py
+++ app/main.py
@@ -19,6 +19,7 @@ from app.distribution_play_routes import router as distribution_play_router
 from app.distribution_routes import router as distribution_router
 from app.growth_autoresearch_routes import router as growth_autoresearch_router
 from app.growth_balance_funding_policy import enable_checkout_first_growth_balance_funding
+from app.growth_balance_rail_ops_routes import router as growth_balance_rail_ops_router
 from app.growth_balance_rail_routes import router as growth_balance_rail_router
 from app.icp_service import icp_service
 from app.logging import configure_logging
@@ -56,6 +57,7 @@ app.include_router(customer_managed_distribution_router)
 app.include_router(customer_reddit_router)
 app.include_router(customer_autoresearch_router)
 app.include_router(growth_balance_rail_router)
+app.include_router(growth_balance_rail_ops_router)
 app.include_router(growth_autoresearch_router)
 app.include_router(distribution_router)
 app.include_router(distribution_play_router)
